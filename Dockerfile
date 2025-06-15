8bf22f1c070544c807fb3b34ae94edeccf6e9b1b
Tatsuya SHORIKI <shouriki-tatsuya@optage.co.jp>
2025-06-16 00:59:08 +0900

    script mode

--- Dockerfile
+++ Dockerfile
@@ -4,17 +4,15 @@ MAINTAINER Serhiy Mitrovtsiy <mitrovtsiy@ukr.net>
 ARG TARGETPLATFORM
 ARG KUBE_VERSION="v1.33.1"
 
-COPY entrypoint.sh /entrypoint.sh
-
 RUN if [ "$TARGETPLATFORM" = "linux/amd64" ]; then ARCHITECTURE=amd64; elif [ "$TARGETPLATFORM" = "linux/arm/v7" ]; then ARCHITECTURE=arm; elif [ "$TARGETPLATFORM" = "linux/arm64" ]; then ARCHITECTURE=arm64; else ARCHITECTURE=amd64; fi && \
-    chmod +x /entrypoint.sh && \
-    apk add --no-cache --update openssl curl ca-certificates && \
+    apk add --no-cache --update openssl curl ca-certificates jq && \
     curl -L https://dl.k8s.io/release/$KUBE_VERSION/bin/linux/$ARCHITECTURE/kubectl -o /usr/local/bin/kubectl && \
     chmod +x /usr/local/bin/kubectl && \
     rm -rf /var/cache/apk/*
-RUN apk add --no-cache --update \
-    jq \
-    && rm -rf /var/cache/apk/*
+
+COPY entrypoint.sh /entrypoint.sh
+COPY scripts /scripts
+RUN chmod +x /entrypoint.sh /scripts/*.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
 CMD ["cluster-info"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,41 +3,15 @@
 set -e
 
 echo "/usr/local/bin/kubectl" >> $GITHUB_PATH
+source /subshell/create-kubeconfig.sh
 
-if [ ! -d "$HOME/.kube" ]; then
-    mkdir -p $HOME/.kube
-fi
-
-if [ ! -f "$HOME/.kube/config" ]; then
-    if [ ! -z "${KUBE_CONFIG}" ]; then
-        echo "$KUBE_CONFIG" | base64 -d > $HOME/.kube/config
-
-        if [ ! -z "${KUBE_CONTEXT}" ]; then
-            kubectl config use-context $KUBE_CONTEXT
-        fi
-    elif [ ! -z "${KUBE_HOST}" ]; then
-        echo "$KUBE_CERTIFICATE" | base64 -d > $HOME/.kube/certificate
-        kubectl config set-cluster default --server=https://$KUBE_HOST --certificate-authority=$HOME/.kube/certificate > /dev/null
-
-        if [ ! -z "${KUBE_PASSWORD}" ]; then
-            kubectl config set-credentials cluster-admin --username=$KUBE_USERNAME --password=$KUBE_PASSWORD > /dev/null
-        elif [ ! -z "${KUBE_TOKEN}" ]; then
-            kubectl config set-credentials cluster-admin --token="${KUBE_TOKEN}" > /dev/null
-        else
-            echo "No credentials found. Please provide KUBE_TOKEN, or KUBE_USERNAME and KUBE_PASSWORD. Exiting..."
-            exit 1
-        fi
-
-        kubectl config set-context default --cluster=default --namespace=default --user=cluster-admin > /dev/null
-        kubectl config use-context default > /dev/null
-    elif [[ $* == "kustomize" ]]; then :;
-    else
-        echo "No authorization data found. Please provide KUBE_CONFIG or KUBE_HOST variables. Exiting..."
-        exit 1
-    fi
-fi
-
-if [ -z "$dest" ]; then
+if [ -n "$script" ]; then
+    echo "$script" > /tmp/script.sh
+    chmod +x /tmp/script.sh
+    cat /tmp/script.sh
+    echo "Running script:"
+    /tmp/script.sh
+elif [ -z "$dest" ]; then
     kubectl $*
 else
     EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
